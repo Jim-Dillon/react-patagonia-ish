0651d7f268ef6143cfcd092db43831dde690a270
Jim-Dillon <james.dillon94@gmail.com>
2024-02-18 14:34:28 +0000

    import patagonia image

--- src/components/Nav/TopNav.tsx
+++ src/components/Nav/TopNav.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent } from "react";
 import HeadButton from "./HeadButton";
 import styles from "/src/css/TopNav.module.scss";
+import image__ArrowDown from '/src/assets/arrow_down.svg';
+import patagonia from '/src/assets/img/Patagonia.png'
 
 
 const TopNav: FunctionComponent = () => {
@@ -11,7 +13,7 @@ const TopNav: FunctionComponent = () => {
                 {/* <img src="src/assets/storybook.svg" alt="" />
                 <p>View all the React components in Storybook</p> */}
                 <p>Sale</p>
-                <img src="src/assets/arrow_down.svg" alt="" />
+                <img src={image__ArrowDown} alt="" />
             </div>
             <div className={styles.header}>
                 <div className={styles.headerLeft}>
@@ -32,7 +34,7 @@ const TopNav: FunctionComponent = () => {
                     <HeadButton imageSrc="src/assets/burger_icon.svg" className={`${styles.headerIcon} ${styles.burgerIcon}`} />
                     <HeadButton imageSrc="src/assets/search_icon.svg" className={`${styles.headerIcon} ${styles.searchIcon}`} />
                 </div>
-                <img src="src/assets/img/Patagonia.png" alt="" className={styles.patLogo}/>
+                <img src={patagonia} alt="" className={styles.patLogo}/>
                 <div className={styles.headerRight}>
                     <HeadButton imageSrc="src/assets/search_icon.svg" className={`${styles.headerIcon} ${styles.searchIcon} ${styles.bigIcon}`} />
                     <HeadButton imageSrc="src/assets/user_icon.svg" className={`${styles.headerIcon} ${styles.userIcon} ${styles.bigIcon}`} />
