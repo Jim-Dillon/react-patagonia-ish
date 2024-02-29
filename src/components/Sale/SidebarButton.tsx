8aa51a4218fd105abbc155db59fb9a3a80d5d854
Jim-Dillon <james.dillon94@gmail.com>
2024-02-29 14:49:55 +0000

    add sidebar button toggle

--- src/components/Sale/SidebarButton.tsx
+++ src/components/Sale/SidebarButton.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, ReactNode } from "react";
+import { FunctionComponent, ReactNode, useState } from "react";
 import SidebarTag from "./SidebarTag";
 import styles from '/src/css/ClothesCard.module.scss'
 import sidebar__ArrowDown from '/src/assets/arrow_down__black.svg';
@@ -11,11 +11,19 @@ interface SidebarButtonProps {
 
 const SidebarButton: FunctionComponent<SidebarButtonProps> = ({ text, sidebarTags, children }) => {
 
+  const [isVisible, setIsVisible] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+    setIsClicked(!isClicked);
+  }
+
   return (
-    <div className={styles.sidebar__button}>
+    <div className={styles.sidebar__button} onClick={toggleVisibility}>
         {text}
-        <img src={sidebar__ArrowDown} alt="" />
-        <ul>
+        <img src={sidebar__ArrowDown} alt="" style={{ transform: isClicked ? 'translateY(-10%) rotate(180deg)' : 'none' }} />
+        <ul style={{ display: isVisible ? 'flex' : 'none' }}>
         {sidebarTags && sidebarTags.map((tag, index) => (
           <SidebarTag
             key={index}
@@ -23,7 +31,9 @@ const SidebarButton: FunctionComponent<SidebarButtonProps> = ({ text, sidebarTag
           />
         ))}
       </ul>
+      <div style={{ display: isVisible ? 'block' : 'none' }}>
         {children}
+      </div>
     </div>
   )
 }
